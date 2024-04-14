8843a6d0ef6de5b5a46b0d377a61b9ac78e35d22
Ihor <ihor.livinskyi@gmail.com>
2024-04-14 13:44:13 +0200

    Homework 8

---
+++ JavaProHome/src/main/java/Main.java
@@ -0,0 +1,166 @@
+import homework8.Product;
+import homework8.ProductBuilder;
+import homework8.ProductManager;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProductManager manager = new ProductManager();
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(300.0)
+                        .setDiscountable(false)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(15.0)
+                        .setDiscountable(false)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(75.0)
+                        .setDiscountable(false)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(74.0)
+                        .setDiscountable(false)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Furniture")
+                        .setPrice(150.0)
+                        .setDiscountable(true)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Furniture")
+                        .setPrice(99.99)
+                        .setDiscountable(true)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Furniture")
+                        .setPrice(25.0)
+                        .setDiscountable(true)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Toy")
+                        .setPrice(13.0)
+                        .setDiscountable(true)
+                        .setCreatedOn(LocalDateTime.now())
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Electronics")
+                        .setPrice(400.0)
+                        .setDiscountable(false)
+                        .setCreatedOn(
+                                LocalDateTime.of(2024, Month.APRIL, 14, 12, 30, 15)
+                        )
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(260.0)
+                        .setDiscountable(true)
+                        .setCreatedOn(
+                                LocalDateTime.of(2024, Month.JANUARY, 1, 10, 10, 30)
+                        )
+                        .createProduct()
+        );
+        manager.addProduct(
+                new ProductBuilder()
+                        .setType("Book")
+                        .setPrice(1.0)
+                        .setDiscountable(true)
+                        .setCreatedOn(
+                                LocalDateTime.of(2023, Month.FEBRUARY, 24, 12, 33, 05)
+                        )
+                        .createProduct()
+        );
+
+        List<Product> filteredByPriceBooks =
+                manager.getProductsByTypeWithPriceHigherThan("Book", 250.0);
+        System.out.println("Books with the price higher than 250:");
+        for (Product book : filteredByPriceBooks) {
+            System.out.println(
+                    "Type: " +
+                            book.getType() +
+                            ", Price: " +
+                            book.getPrice() +
+                            " EUR"
+            );
+        }
+
+        List<Product> discountedBooks = manager.getDiscountedProductsWithType(
+                "Book"
+        );
+        System.out.println("Get books with discount:");
+        for (Product book : discountedBooks) {
+            System.out.println(
+                    "Type: " +
+                            book.getType() +
+                            ", Price: " +
+                            book.getPrice() +
+                            "EUR "
+            );
+        }
+
+        try {
+            Product cheapestBook = manager.getCheapestProduct("Cat");
+            System.out.println(
+                    "Cheapest book price: " + cheapestBook.getPrice()
+            );
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            Product cheapestBook = manager.getCheapestProduct("Book");
+            System.out.println(
+                    "Cheapest book price: " + cheapestBook.getPrice()
+            );
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        List<Product> latestProducts = manager.getThreeLatestProducts();
+        for (Product latestBook : latestProducts) {
+            System.out.println(latestBook.getCreatedOn());
+        }
+
+        System.out.println(
+                manager.getThisYearProductsTotalPriceWithPriceLowerThanSpecified(
+                        75.0
+                )
+        );
+        var sortedProducts = manager.groupProductsByType();
+        System.out.println(sortedProducts.toString());
+    }
+}
---
+++ JavaProHome/src/main/java/homework8/ProductManager.java
@@ -0,0 +1,99 @@
+package homework8;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ProductManager {
+
+    private int lastProductNumber = 0;
+    private List<Product> products;
+
+    public ProductManager() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        product.setId(++lastProductNumber);
+        products.add(product);
+    }
+
+    public List<Product> getProductsByTypeWithPriceHigherThan(
+            String type,
+            Double price
+    ) {
+        return products
+                .stream()
+                .filter(product ->
+                        product.getType().equals(type) && product.getPrice() > price
+                )
+                .toList();
+    }
+
+    public List<Product> getDiscountedProductsWithType(String type) {
+        return products
+                .stream()
+                .filter(product ->
+                        product.getType().equals(type) && product.getDiscountable()
+                )
+                .map(product -> {
+                    double discountedPrice = product.getPrice() * 0.9;
+                    product.setPrice(discountedPrice);
+                    return product;
+                })
+                .toList();
+    }
+
+    public Product getCheapestProduct(String type)
+            throws NoSuchElementException {
+        Optional<Product> cheapestBook = products
+                .stream()
+                .filter(product -> product.getType().equals(type))
+                .min(Comparator.comparingDouble(Product::getPrice));
+        if (cheapestBook.isEmpty()) {
+            throw new NoSuchElementException(
+                    "Продукт [категорія: " + type + "] не знайдено"
+            );
+        }
+        return cheapestBook.get();
+    }
+
+    public List<Product> getThreeLatestProducts() {
+        return products
+                .stream()
+                .sorted(Comparator.comparing(Product::getCreatedOn).reversed())
+                .limit(3)
+                .toList();
+    }
+
+    public Double getThisYearProductsTotalPriceWithPriceLowerThanSpecified(
+            Double maxPrice
+    ) {
+        return products
+                .stream()
+                .filter(product ->
+                        product
+                                .getCreatedOn()
+                                .isAfter(
+                                        LocalDateTime.of(
+                                                2024,
+                                                Month.from(Month.JANUARY),
+                                                1,
+                                                0,
+                                                0,
+                                                0
+                                        )
+                                ) &&
+                                product.getPrice() < maxPrice
+                )
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
+
+    public Map<String, List<Product>> groupProductsByType() {
+        return products
+                .stream()
+                .collect(Collectors.groupingBy(Product::getType));
+    }
+}
