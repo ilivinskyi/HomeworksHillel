b5655be832d861513b12e6edb06b39a5fa4f3056
Ihor <ihor.livinskyi@gmail.com>
2024-04-18 12:40:35 +0200

    code style fix

--- JavaProHome/src/main/java/Main.java
+++ JavaProHome/src/main/java/Main.java
@@ -1,140 +1,36 @@
-import homework8.Product;
-import homework8.ProductBuilder;
-import homework8.ProductManager;
-
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.List;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        ProductManager manager = new ProductManager();
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(300.0)
-                        .setDiscountable(false)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(15.0)
-                        .setDiscountable(false)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(75.0)
-                        .setDiscountable(false)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(74.0)
-                        .setDiscountable(false)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Furniture")
-                        .setPrice(150.0)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Furniture")
-                        .setPrice(99.99)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Furniture")
-                        .setPrice(25.0)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Toy")
-                        .setPrice(13.0)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.now())
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Electronics")
-                        .setPrice(400.0)
-                        .setDiscountable(false)
-                        .setCreatedOn(LocalDateTime.of(2024, Month.APRIL, 14, 12, 30, 15))
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(260.0)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.of(2024, Month.JANUARY, 1, 10, 10, 30))
-                        .createProduct()
-        );
-        manager.addProduct(
-                new ProductBuilder()
-                        .setType("Book")
-                        .setPrice(1.0)
-                        .setDiscountable(true)
-                        .setCreatedOn(LocalDateTime.of(2023, Month.FEBRUARY, 24, 12, 33, 05))
-                        .createProduct()
-        );
+        int[] testOne = {1, 2, 3};
+        int[] testTwo = {1, 2, 3, 3};
+        int[] testThree = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
 
-        List<Product> filteredByPriceBooks = manager.getProductsByTypeWithPriceGT(
-                "Book",
-                250.0
-        );
-        System.out.println("Books with the price higher than 250:");
-        for (Product book : filteredByPriceBooks) {
-            System.out.println("Type: " + book.getType() + ", Price: " + book.getPrice() + " EUR");
+        System.out.println(containsDuplicate(testOne));
+        System.out.println(containsDuplicate(testTwo));
+        System.out.println(containsDuplicate(testThree));
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        var set = new HashSet<Integer>();
+        for (int item : nums) {
+            if (!set.add(item)) {
+                return true;
+            }
         }
+        return false;
+    }
 
-        List<Product> discountedBooks = manager.getDiscountedProductsWithType("Book");
-        System.out.println("Get books with discount:");
-        for (Product book : discountedBooks) {
-            System.out.println("Type: " + book.getType() + ", Price: " + book.getPrice() + "EUR ");
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return false;
         }
-
-        try {
-            Product cheapestBook = manager.getCheapestProduct("Cat");
-            System.out.println("Cheapest book price: " + cheapestBook.getPrice());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-
-        try {
-            Product cheapestBook = manager.getCheapestProduct("Book");
-            System.out.println("Cheapest book price: " + cheapestBook.getPrice());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-
-        List<Product> latestProducts = manager.getThreeLatestProducts();
-        for (Product latestBook : latestProducts) {
-            System.out.println(latestBook.getCreatedOn());
-        }
-
-        System.out.println(manager.getThisYearProductsTotalPriceWithPriceLowerThanSpecified(75.0));
-        var sortedProducts = manager.groupProductsByType();
-        System.out.println(sortedProducts.toString());
+        var inputString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        var reversedString = new StringBuilder(inputString).reverse().toString();
+        return inputString.equals(reversedString);
     }
 }
