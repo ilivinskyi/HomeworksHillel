2e0a06936efa6dda6aa2e473487ea3effba3fcd0
Ihor <ihor.livinskyi@gmail.com>
2024-05-18 08:17:42 +0200

    Homework 14

---
+++ JavaProHome/src/test/java/unittesting/JewelsAndStonedSolverTest.java
@@ -0,0 +1,60 @@
+package unittesting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JewelsAndStonedSolverTest {
+
+    JewelsAndStonedSolver solver;
+
+    @BeforeEach
+    void setUp() {
+        solver = new JewelsAndStonedSolver();
+    }
+
+    @Test
+    void testHasValidJewels() {
+        var jewels = "aA";
+        var stones = "aAAbbbb";
+        var expected = 3;
+        var actual = solver.numJewelsInStones(jewels, stones);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDoesntHaveValidJewels() {
+        var jewels = "cC";
+        var stones = "aAAbbbb";
+        var expected = 0;
+        var actual = solver.numJewelsInStones(jewels, stones);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testHasOnlyJewels() {
+        var jewels = "aA";
+        var stones = "";
+        var expected = 0;
+        var actual = solver.numJewelsInStones(jewels, stones);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testHasOnlyStones() {
+        var jewels = "0";
+        var stones = "aAAbbbb";
+        var expected = 0;
+        var actual = solver.numJewelsInStones(jewels, stones);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testEmptyValues() {
+        var jewels = "";
+        var stones = "";
+        var expected = 0;
+        var actual = solver.numJewelsInStones(jewels, stones);
+        Assertions.assertEquals(expected, actual);
+    }
+}
