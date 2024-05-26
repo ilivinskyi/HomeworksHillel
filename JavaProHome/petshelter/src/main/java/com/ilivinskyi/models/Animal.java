98c3941eb80dcaf7dfd3a9fe97599c5a926ba5ee
Ihor <ihor.livinskyi@gmail.com>
2024-05-26 14:04:33 +0200

    modules refactoring

---
+++ JavaProHome/petshelter/src/main/java/com/ilivinskyi/models/Animal.java
@@ -0,0 +1,48 @@
+package main.java.models;
+
+public class Animal {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Animal() {}
+
+    public Animal(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
